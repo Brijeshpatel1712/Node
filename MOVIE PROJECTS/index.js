435190d324fdbde4b0446a8da95f777aceae0902
Brijeshpatel1712 <brijeshsangani088@gmail.com>
2025-11-19 08:24:24 +0530

    Refactor user deletion endpoint and clean up code

--- MOVIE PROJECTS/index.js
+++ MOVIE PROJECTS/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { User } = require('./db');
-
 const app = express();
 const PORT = 2020;
 
@@ -19,38 +18,22 @@ app.post('/users', async (req, res) => {
   try {
     const { name, email, age } = req.body;
     const user = new User({ name, email, age });
-
     await user.save();
     res.status.json({ message: 'User created', user });
   } catch (err) {
     res.status.json({ message: 'Error creating user' });
   }
 });
-app.delete('/deleteproduct/:id',(req,res)=>{
-    const {id} = req.params
-    fs.readFile('./db.json',"utf-8",(err,data)=>{
-        if(err)
-        {
-            res.send(err)
-        }
-        else
-        {
-            const dataFromdb = JSON.parse(data)
-            const filterProduct = dataFromdb.product.filter((el)=>el.id!=id)
-            fs.writeFile('./db.json',JSON.stringify({product:filterProduct}),(err)=>{
-                if(err)
-                {
-                    res.send(err)
-                }
-                else
-                {
-                    res.send("Data is deleted")
-                }
-            })
-        }
-    })
-    res.send("Data Deleted")
-})
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.status.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status.json({ message: 'Error deleting user' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
